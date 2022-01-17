dddbc3126e83ad6eb90712350ae97f0ba457a7b4
kaiopiola <kaio.piola@hotmail.com>
2022-01-17 19:20:22 -0300

    Removes unnecessary "for" loop - Closes #5

--- src/Key.php
+++ src/Key.php
@@ -19,7 +19,7 @@ class Key extends Settings
     protected $numbers = '0123456789';
     protected $letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     protected $pattern = 'XXXXX-XXXXX-XXXXX';
-
+    
     public function generate()
     {
         mb_internal_encoding('UTF-8');
@@ -28,31 +28,25 @@ class Key extends Settings
         $numbers = $this->numbers;
         $letters = $this->letters;
         $characters = $numbers . $letters;
-
         $pattern = $this->pattern;
-        $divide_pattern = explode('-', $pattern);
-
+        
         $generated_key = "";
-        for ($i = 0; $i < count($divide_pattern); $i++) {
-            $randstring = "";
-            for ($x = 0; $x < mb_strlen($divide_pattern[$i]); $x++) {
-                switch ($divide_pattern[$i][$x]) {
-                    case "N":
-                        $randstring .= $numbers[rand(0, mb_strlen($numbers) - 1)];
-                        break;
-                    case "L":
-                        $randstring .= $letters[rand(0, mb_strlen($letters) - 1)];
-                        break;
-                    case "X":
-                        $randstring .= $characters[rand(0, mb_strlen($characters) - 1)];
-                        break;
-                    default:
-                        // $randstring .= $characters[rand(0, strlen($characters) - 1)];
-                        $randstring .= $divide_pattern[$i][$x];
-                        break;
-                }
+        for ($x = 0; $x < mb_strlen($pattern); $x++) {
+            switch ($pattern[$x]) {
+                case "N":
+                    $generated_key .= $numbers[rand(0, mb_strlen($numbers) - 1)];
+                    break;
+                case "L":
+                    $generated_key .= $letters[rand(0, mb_strlen($letters) - 1)];
+                    break;
+                case "X":
+                    $generated_key .= $characters[rand(0, mb_strlen($characters) - 1)];
+                    break;
+                default:
+                    // $randstring .= $characters[rand(0, strlen($characters) - 1)];
+                    $generated_key .= $pattern[$x];
+                    break;
             }
-            $i == count($divide_pattern) - 1 ? $generated_key .= $randstring : $generated_key .= $randstring . "-";
         }
         return $generated_key;
     }
